2da583189f9ec017fc6ae594ce8d7715a9dd1b8c
phartenfeller <pharti@hotmail.de>
2021-04-26 14:25:14 +0200

    newsletter regristration now active

--- src/components/landing-page/NewsletterModal.jsx
+++ src/components/landing-page/NewsletterModal.jsx
@@ -6,7 +6,11 @@ import React, { Fragment, useEffect, useState } from 'react';
 import isEmail from 'validator/lib/isEmail';
 import usePlausible from '../../hooks/usePlausible';
 
+const approvalId = `newsletter_modal_approval`;
+const emailId = `newsletter_modal_email`;
+
 const NewsletterModal = ({ open, closeModal, email, setEmail }) => {
+  const [error, setError] = useState('');
   const validEmail = email === '' || isEmail(email);
 
   const [approval, setApproval] = useState(false);
@@ -14,6 +18,29 @@ const NewsletterModal = ({ open, closeModal, email, setEmail }) => {
   const buttonDisabled = !validEmail || !approval || email === '';
 
   const plausible = usePlausible();
+
+  const handleSubscribeClick = () => {
+    plausible('Newsletter-Subscribe');
+
+    const custHeaders = new Headers();
+    custHeaders.append('Content-Type', 'application/json');
+    const requestOptions = {
+      method: 'POST',
+      headers: custHeaders,
+      body: JSON.stringify({ email }),
+      redirect: 'follow',
+    };
+
+    fetch('https://newsletter.lct.software/register', requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        // eslint-disable-next-line no-console
+        console.log(result);
+        setEmail('');
+        closeModal();
+      })
+      .catch((err) => setError(`${err}`));
+  };
 
   // log opening of newsletter
   useEffect(() => {
@@ -73,86 +100,85 @@ const NewsletterModal = ({ open, closeModal, email, setEmail }) => {
                   >
                     Subscribe to our Newsletter
                   </Dialog.Title>
-                  <form action="#">
-                    <div className="mt-5 mx-3">
-                      <div className="min-w-0 flex-1">
-                        <label htmlFor="email" className="sr-only">
-                          Email address
-                        </label>
-                        <input
-                          id="email"
-                          type="email"
-                          placeholder="Enter your email"
-                          className={`block w-full px-4 py-3 rounded-md border-0 text-base bg-gray-700 text-gray-100 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lightBlue-500 ${
-                            !validEmail
-                              ? 'ring-inset ring-2 ring-red-500'
-                              : null
-                          }`}
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <span
-                          className={`text-sm ${
-                            validEmail ? 'invisible' : `text-red-300`
-                          }`}
-                        >
-                          Please provide a valid E-Mail
-                        </span>
-                      </div>
-                      <div className="max-w-lg mt-3">
-                        <div className="relative flex items-start">
-                          <div className="flex items-center h-5 my-auto">
-                            <input
-                              id="approval"
-                              name="approval"
-                              type="checkbox"
-                              className="focus:ring-lightBlue-500 focus:ring-offset-gray-700 h-4 w-4 text-lightBlue-600 bg-gray-700 border-gray-600 rounded"
-                              value={approval}
-                              onChange={(e) => setApproval(e.target.checked)}
-                            />
-                          </div>
-                          <div className="ml-3 text-sm">
-                            <label
-                              htmlFor="approval"
-                              className="font-medium text-gray-200"
-                            >
-                              Approval
-                              <p className="text-gray-400 font-light">
-                                I accept that my submitted data is processed and
-                                saved in a database. My email address can be
-                                used as a contact possibility (this approvement
-                                can always be reclaimed through a formless
-                                notification).
-                              </p>
-                            </label>
+                  <div className="mt-5 mx-3">
+                    <div className="min-w-0 flex-1">
+                      <label htmlFor={emailId} className="sr-only">
+                        Email address
+                      </label>
+                      <input
+                        id={emailId}
+                        type="email"
+                        placeholder="Enter your email"
+                        className={`block w-full px-4 py-3 rounded-md border-0 text-base bg-gray-700 text-gray-100 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lightBlue-500 ${
+                          !validEmail ? 'ring-inset ring-2 ring-red-500' : null
+                        }`}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <span
+                        className={`text-sm ${
+                          validEmail ? 'invisible' : `text-red-300`
+                        }`}
+                      >
+                        Please provide a valid E-Mail
+                      </span>
+                    </div>
+                    <div className="max-w-lg mt-3">
+                      <div className="relative flex items-start">
+                        <div className="flex items-center h-5 my-auto">
+                          <input
+                            id={approvalId}
+                            name="approval"
+                            type="checkbox"
+                            className="focus:ring-lightBlue-500 focus:ring-offset-gray-700 h-4 w-4 text-lightBlue-600 bg-gray-700 border-gray-600 rounded"
+                            value={approval}
+                            onChange={(e) => setApproval(e.target.checked)}
+                          />
+                        </div>
+                        <div className="ml-3 text-sm">
+                          <label
+                            htmlFor={approvalId}
+                            className="font-medium text-gray-200"
+                          >
+                            Approval
                             <p className="text-gray-400 font-light">
-                              More information can be found in our{' '}
-                              <a
-                                href="https://www.mt-ag.com/datenschutz/"
-                                className="text-gray-200 hover:underline focus:text-gray-400"
-                              >
-                                privacy policy
-                              </a>
-                              .
+                              I accept that my submitted data is processed and
+                              saved in a database. My email address can be used
+                              as a contact possibility (this approvement can
+                              always be reclaimed through a formless
+                              notification).
                             </p>
-                          </div>
+                          </label>
+                          <p className="text-gray-400 font-light">
+                            More information can be found in our{' '}
+                            <a
+                              href="https://www.mt-ag.com/datenschutz/"
+                              className="text-gray-200 hover:underline focus:text-gray-400"
+                            >
+                              privacy policy
+                            </a>
+                            .
+                          </p>
                         </div>
                       </div>
                     </div>
-                  </form>
+                  </div>
                 </div>
+                {error ? (
+                  <div className="text-red-400 mt-4">Error: {error}</div>
+                ) : null}
               </div>
               <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
                 <button
                   type="button"
                   className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-lightBlue-600 text-base font-medium text-white hover:bg-lightBlue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lightBlue-400 focus:ring-offset-gray-700 sm:col-start-2 sm:text-sm disabled:bg-lightBlue-900 disabled:cursor-not-allowed disabled:text-gray-500"
-                  onClick={() => closeModal()}
+                  onClick={handleSubscribeClick}
                   disabled={buttonDisabled}
                 >
                   Subscribe
                 </button>
                 <button
-                  type="submit"
+                  type="button"
                   className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-600 shadow-sm px-4 py-2 bg-gray-700 text-base font-medium text-gray-300 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 focus:ring-offset-gray-700 sm:mt-0 sm:col-start-1 sm:text-sm"
                   onClick={() => closeModal()}
                 >
