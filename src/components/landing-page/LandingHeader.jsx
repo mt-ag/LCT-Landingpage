dbe8da57d32fe7eccfa7ae576395f646452da4c6
phartenfeller <pharti@hotmail.de>
2022-03-13 09:48:01 +0100

    blog link in landing header

--- src/components/landing-page/LandingHeader.jsx
+++ src/components/landing-page/LandingHeader.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import MTLogo from '../../svgs/mt-ag-logo.svg';
 
 const LandingHeader = () => (
   <header className="flex justify-between py-4 px-4 lg:px-0">
-    <h1 className="text-xl font-bold text-zinc-100">Low Code Testing</h1>
+    <div className="flex items-center space-x-5">
+      <h1 className="text-xl font-bold text-zinc-100">
+        <span className="hidden md:block">Low Code Testing</span>
+        <span className="md:hidden">LCT</span>
+      </h1>
+      <Link
+        to="/blog"
+        className="rounded px-2 py-1 text-zinc-300 hover:bg-zinc-700/50 focus:outline-none focus:ring focus:ring-cyan-300/70"
+      >
+        Blog
+      </Link>
+    </div>
     <div className="flex items-center">
       <a
         href="https://www.mt-ag.com/"
