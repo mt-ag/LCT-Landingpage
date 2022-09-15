908ef63fc567bd1394745a61121dbffb9b6a1eb9
phartenfeller <pharti@hotmail.de>
2022-09-15 11:46:28 +0200

    fix external storage name

--- src/store/useExternalContentPrefs.js
+++ src/store/useExternalContentPrefs.js
@@ -2,13 +2,19 @@ import create from 'zustand';
 import { persist } from 'zustand/middleware';
 
 const useExternalContentPrefs = create(
-  persist((set) => ({
-    youTube: false,
-    setYouTube: (b) => {
-      set(() => ({
-        youTube: b,
-      }));
-    },
-  }))
+  persist(
+    (set) => ({
+      youTube: false,
+      setYouTube: (b) => {
+        set(() => ({
+          youTube: b,
+        }));
+      },
+    }),
+    {
+      name: 'external-content-prefs-storage',
+      getStorage: () => localStorage,
+    }
+  )
 );
 export default useExternalContentPrefs;
