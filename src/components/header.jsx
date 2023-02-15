572c4a54eee56aaaf876a0226d687f772b8877d1
phartenfeller <pharti@hotmail.de>
2023-02-15 10:36:00 +0100

    updated non index header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'gatsby';
 import React, { useEffect } from 'react';
 import { SunIcon, MoonIcon } from '@heroicons/react/outline';
-import MTLogo from '../svgs/mt-ag-logo.svg';
+import MTLogo from '../svgs/mt-logo.svg';
 import LCTLogo from '../svgs/lct-logo.svg';
 import useDarkmodeStore from '../store/useDarkmodeStore';
 import '../styles/headerStyles.css';
@@ -20,10 +20,10 @@ const Header = () => {
   });
 
   return (
-    <header className="sticky top-0 z-50 flex justify-between border-b border-zinc-200 bg-cyan-300/75 px-1 backdrop-blur dark:border-slate-800 dark:bg-slate-800/75 md:p-3 lg:px-4">
+    <header className="sticky top-0 z-50 flex justify-between border-b border-zinc-200 bg-mt-blue px-1 backdrop-blur dark:border-slate-800 dark:bg-slate-800/75 md:p-3 lg:px-4">
       <div className="flex items-center space-x-5">
         <Link
-          className="rounded px-2 py-1 font-bold text-zinc-900 transition-colors hover:bg-cyan-100/50 focus:outline-none focus:ring focus:ring-cyan-500/50 dark:text-cyan-200 dark:hover:bg-slate-700/70"
+          className="rounded px-2 py-1 font-bold text-zinc-200 transition-colors hover:bg-mt-old-blue/50 focus:outline-none focus:ring focus:ring-cyan-500/50 dark:text-cyan-200 dark:hover:bg-slate-700/70"
           to="/"
         >
           <div className="flex items-center">
@@ -34,10 +34,18 @@ const Header = () => {
         </Link>
         <Link
           to="/blog"
-          className="rounded px-2 py-1 text-slate-700 transition-colors hover:bg-cyan-100/50 focus:outline-none focus:ring focus:ring-cyan-500/50 dark:text-slate-300 dark:hover:bg-slate-700/70"
+          className="rounded px-2 py-1 text-slate-300 transition-colors hover:bg-mt-old-blue/50 focus:outline-none focus:ring focus:ring-cyan-500/50 dark:hover:bg-slate-700/70"
         >
           Blog
         </Link>
+        <a
+          href="https://www.youtube.com/@lct-apex"
+          target="_blank"
+          rel="noopener"
+          className="rounded px-2 py-1 text-slate-300 transition-colors hover:bg-mt-old-blue/50 focus:outline-none focus:ring focus:ring-cyan-500/50 dark:hover:bg-slate-700/70"
+        >
+          Videos
+        </a>
       </div>
       <div className="flex items-center">
         <button
@@ -45,8 +53,8 @@ const Header = () => {
           onClick={toggleDarkMode}
           title={`switch to ${isDark ? 'light' : 'dark'} mode`}
           className={classNames(
-            'mr-6 rounded px-2 py-1 text-zinc-700 hover:bg-cyan-100/50 focus:outline-none focus:ring focus:ring-cyan-600/50 dark:text-slate-400 dark:hover:bg-slate-700/70',
-            'transition-colors duration-300 hover:text-indigo-600 dark:hover:text-amber-400'
+            'mr-6 rounded px-2 py-1  text-slate-400 hover:bg-mt-old-blue/50 focus:outline-none focus:ring focus:ring-cyan-500/50 dark:hover:bg-slate-700/70',
+            'transition-colors duration-300 hover:text-indigo-300 dark:hover:text-amber-400'
           )}
         >
           {isDark ? (
@@ -57,11 +65,11 @@ const Header = () => {
         </button>
         <a
           href="https://www.mt-itsolutions.com/"
-          className="hidden px-2 lg:block"
+          className="hidden px-2 focus:outline-none focus:ring focus:ring-cyan-500/50 lg:block"
           aria-label="MT AG"
         >
           <div>
-            <MTLogo className="h-[28px] w-auto text-zinc-800 dark:text-slate-200" />
+            <MTLogo className="h-[28px] w-auto " />
           </div>
         </a>
       </div>
