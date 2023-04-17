f0d4fb01cc16194c9997b42db0a94a6fcc7dfb4f
phartenfeller <pharti@hotmail.de>
2023-04-17 16:17:21 +0200

    fix: added missing import

--- src/store/useExternalContentPrefs.js
+++ src/store/useExternalContentPrefs.js
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 const useExternalContentPrefs = create(
   persist(
