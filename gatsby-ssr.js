7072a2e89bf611838d55992f64b7b56d40a69c2f
ItIsFabian <fa.al.neu@gmail.com>
2025-02-12 11:01:43 +0100

    added litlyx script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,16 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require('react')
 
-// You can delete this file if you're not using it
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+setPostBodyComponents([
+
+  // If you want to load an external script:
+  React.createElement('script', {
+    defer: true,
+    'data-project': '67ac5fa1ff3be229dde663f1',
+    'data-host': 'producer.kai-donato.de',
+    'data-port': '443',
+    'data-secure': 'true',
+    src: 'https://cdn.jsdelivr.net/gh/litlyx/litlyx-js/browser/litlyx.js'
+  })
+])
+}
