0f863caa0efbc3262c5bc58c9af1b74b0829395f
Chris Röhrs <122437451+chrisroehrs@users.noreply.github.com>
2025-04-25 18:08:57 +0200

    Update pricing section styling

--- src/components/landing-page/Offer.jsx
+++ src/components/landing-page/Offer.jsx
@@ -40,13 +40,13 @@ const PriceBlock = () => {
   }
 
   return (
-    <div className="rounded-3xl bg-white p-8 ring-2 ring-mt-blue/75 xl:p-10">
+    <div className="rounded-3xl p-8 ring-2 ring-hyand-blue xl:p-10">
       <div className="flex items-center justify-between gap-x-4">
-        <h3 className="text-lg font-semibold leading-8 text-black">
+        <h3 className="text-lg font-semibold leading-8 text-white">
           {noSupport ? 'LCT Usage Fee (no Support)' : 'LCT With Support Hours'}
         </h3>
       </div>
-      <p className="mt-4 text-sm leading-6 text-gray-600">
+      <p className="mt-4 text-sm leading-6 text-white">
         Get access to LCT with a monthly subscription fee.
       </p>
 
@@ -61,20 +61,20 @@ const PriceBlock = () => {
           className="x-accent-color h-2 w-full cursor-ew-resize appearance-none rounded-full bg-gray-200 disabled:cursor-not-allowed"
         />
         <div className="-mt-1 flex w-full justify-between">
-          <span className="text-sm text-gray-600">0</span>
-          <span className="text-sm text-gray-600">100</span>
+          <span className="text-sm text-white">0</span>
+          <span className="text-sm text-white">100</span>
         </div>
       </div>
 
       <p className="mt-6 flex items-baseline gap-x-1">
-        <span className="text-4xl font-bold tracking-tight text-gray-900">
+        <span className="text-4xl font-bold tracking-tight text-white">
           {`${kSep(500 + sliderVal * 100)} €`}
         </span>
-        <span className="text-sm font-semibold leading-6 text-gray-600">
+        <span className="text-sm font-semibold leading-6 text-gray-400">
           month
         </span>
       </p>
-      <ul className="mt-8 space-y-3 text-sm leading-6 text-gray-600 xl:mt-10">
+      <ul className="mt-8 space-y-3 text-sm leading-6 text-white xl:mt-10">
         {features.map((feature) => (
           <li key={feature} className="flex gap-x-3">
             <CheckCircleIcon
@@ -142,9 +142,9 @@ const WorkshopBlock = () => {
   }
 
   return (
-    <div className="rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10">
+    <div className="rounded-3xl p-8 ring-1 ring-gray-200 xl:p-10">
       <div className="flex items-center justify-between gap-x-4">
-        <h3 className="text-lg font-semibold leading-8 text-black">
+        <h3 className="text-lg font-semibold leading-8 text-white">
           Jump Start Workshop
         </h3>
 
@@ -153,7 +153,7 @@ const WorkshopBlock = () => {
         </span>
       </div>
 
-      <p className="mt-4 text-sm leading-6 text-gray-600">
+      <p className="mt-4 text-sm leading-6 text-white">
         Kickstart your LCT journey with a workshop by our team for a swift and
         efficient start.
       </p>
@@ -169,9 +169,9 @@ const WorkshopBlock = () => {
               value={option}
               className={({ active, checked }) =>
                 classNames(
-                  active ? 'ring-2 ring-mt-blue ring-offset-2' : '',
+                  active ? 'ring-2 ring-hyand-blue ring-offset-2' : '',
                   checked
-                    ? 'bg-mt-blue text-white hover:bg-mt-blue/80'
+                    ? 'bg-hyand-blue text-white hover:bg-hyand-blue/80'
                     : 'bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50',
                   'flex cursor-pointer select-none items-center justify-center rounded-md px-3 py-3 text-sm font-semibold uppercase focus:outline-none sm:flex-1'
                 )
@@ -184,19 +184,19 @@ const WorkshopBlock = () => {
       </RadioGroup>
 
       <p className="mt-6 flex items-baseline gap-x-1">
-        <span className="text-4xl font-bold tracking-tight text-gray-900">
+        <span className="text-4xl font-bold tracking-tight text-white">
           {`${kSep(price)} €`}
         </span>
-        <span className="text-sm font-semibold leading-6 text-gray-600">
+        <span className="text-sm font-semibold leading-6 text-gray-400">
           once
         </span>
       </p>
 
-      <p className="mt-2 text-gray-700">
+      <p className="mt-2 text-white">
         Workshop length: <span className="font-semibold">{days} days</span>
       </p>
 
-      <ul className="mt-8 space-y-3 text-sm leading-6 text-gray-600 xl:mt-10">
+      <ul className="mt-8 space-y-3 text-sm leading-6 text-white xl:mt-10">
         {basicWsFeatures.map((feature) => (
           <li key={feature} className="flex gap-x-3">
             <CheckCircleIcon
@@ -281,7 +281,7 @@ const Offer = () => {
 
         <div className="mx-6 mt-12 text-center lg:mx-0">
           <a
-            className="text-gray-700 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-mt-blue focus:ring-offset-2 focus:ring-offset-white"
+            className="text-gray-300 underline hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-mt-blue focus:ring-offset-2 focus:ring-offset-white"
             href="https://apex.mt-itsolutions.com/ords/f?p=APEXSUPPORT:3:0&p_lang=en"
           >
             We also offer consulting services to help with any APEX related
