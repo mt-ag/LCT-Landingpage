4b69080786006165d6e39b4132a351aa9e88fd40
Chris Röhrs <122437451+chrisroehrs@users.noreply.github.com>
2025-05-08 15:15:06 +0200

    Remove unused `mt-` colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'mt-blue': '#00476f',
-        'mt-green': '#6ba055',
-        'mt-darkgreen': '#436e5e',
         'hyand-black': '#000000',
         'hyand-blue': '#0092e1',
         'hyand-green': '#00c48d',
