b442da91d4e0ce402b85415f6519c5c4e00c1da0
Chris Röhrs <122437451+chrisroehrs@users.noreply.github.com>
2025-06-27 10:29:51 +0200

    Code formatting

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,19 +1,19 @@
 const React = require('react');
+
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
 exports.onRenderBody = ({ setPostBodyComponents }) => {
-setPostBodyComponents([
-
-  // If you want to load an external script:
-  React.createElement('script', {
-    defer: true,
-    'data-project': process.env.TRACKER_KEY,
-    'data-host': process.env.TRACKER_HOST,
-    'data-port': '443',
-    'data-secure': 'true',
-    src: 'https://cdn.jsdelivr.net/gh/litlyx/litlyx-js/browser/litlyx.js'
-  })
-])
-}
+  setPostBodyComponents([
+    // If you want to load an external script:
+    React.createElement('script', {
+      defer: true,
+      'data-project': process.env.TRACKER_KEY,
+      'data-host': process.env.TRACKER_HOST,
+      'data-port': '443',
+      'data-secure': 'true',
+      src: 'https://cdn.jsdelivr.net/gh/litlyx/litlyx-js/browser/litlyx.js',
+    }),
+  ]);
+};
