e16938b0ba17e338c4c5fb034587492c31705faa
Chris Röhrs <122437451+chrisroehrs@users.noreply.github.com>
2025-11-14 10:38:49 +0100

    Update contact information of Kai Donato

--- src/components/landing-page/Contact.jsx
+++ src/components/landing-page/Contact.jsx
@@ -2,6 +2,7 @@ import { MailIcon } from '@heroicons/react/outline';
 import React, { useEffect, useState } from 'react';
 import decodeMail from '../../util/decodeMail';
 import ImageGetter from '../ImageGetter';
+import svg from '../../svgs/link-to-svg';
 
 const Contact = () => {
   const [email, setEmail] = useState('loading...');
@@ -41,11 +42,11 @@ const Contact = () => {
                   <p className="text-2xl font-semibold tracking-tight text-hyand-white lg:text-2xl">
                     Kai Donato
                   </p>
-                  <p className="mt-2 text-white">Product Manager LCT</p>
-                  <div className="inline-block">
+                  <p className="mt-2 mb-2 lg:mb-5 text-white">Product Manager LCT</p>
+                  <div className="block">
                     <a
-                      href="https://twitter.com/_kaidonato"
-                      className="group mt-2 flex justify-start rounded py-2 pr-3 text-white focus:ring-2 focus:ring-sky-500 lg:mt-5"
+                      href="https://www.linkedin.com/in/kaidonato"
+                      className="group flex justify-start rounded py-2 pr-3 text-white focus:ring-2 focus:ring-sky-500"
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -54,9 +55,27 @@ const Contact = () => {
                         fill="currentColor"
                         className="mr-3 text-zinc-400 group-hover:text-hyand-blue"
                       >
-                        <path d="M24 4.557a9.83 9.83 0 01-2.828.775 4.932 4.932 0 002.165-2.724 9.864 9.864 0 01-3.127 1.195a4.916 4.916 0 00-3.594-1.555c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 3.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084a4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 19.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 4.557z" />
+                        <path d={svg.linkedin.path} />
                       </svg>
-                      @_KaiDonato
+                      Kai Donato
+                    </a>
+                  </div>
+                  <div className="block">
+                    <a
+                      href="https://bsky.app/profile/kaidonato.bsky.social"
+                      className="group flex justify-start rounded py-2 pr-3 text-white focus:ring-2 focus:ring-sky-500"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 320 300"
+                        fill="currentColor"
+                        className="mr-3 text-zinc-400 group-hover:text-hyand-blue"
+                      >
+                        <path d={svg.bluesky.path} />
+                      </svg>
+                      @kaidonato.bsky.social
                     </a>
                   </div>
                 </div>
